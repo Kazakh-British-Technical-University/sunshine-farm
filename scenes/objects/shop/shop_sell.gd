96a758a0259f01f2aa7a5fbcfb882da63f230cf6
Dmitriy Tuchashvili <pechorin211@gmail.com>
2025-02-20 11:35:12 +0500

    added Sell mechanic

---
+++ scenes/objects/shop/shop_sell.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+@export var item_for_sale: InventoryItem
+
+var _gold = load("res://resources/item/gold.tres")
+var _empty = load("res://resources/item/empty.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$InteractableComponent.interacted.connect(_on_interacted)
+	
+	
+func _on_interacted(origin):
+	var inventory = origin.get_node("InventoryComponent")
+	if (inventory == null):
+		return
+		
+	var current_item = inventory.current_item
+	if (current_item == _empty && current_item != _gold):
+		return
+		
+	if (inventory.try_spend_item(current_item, 1)):
+		inventory.add_item(_gold, current_item.price)
