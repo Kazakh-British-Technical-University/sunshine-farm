3db2d3912e57beb0df9af1d45aeb9e2edd8a2871
Dmitriy Tuchashvili <pechorin211@gmail.com>
2025-02-20 11:15:14 +0500

    added ui to check current item

---
+++ scenes/ui/current_item_ui.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var _inventory
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_inventory = Globals.player.get_node("InventoryComponent")
+	_inventory.current_item_changed.connect(_on_current_item_changed)
+	_inventory.items_updated.connect(_on_items_updated)
+	
+
+func _on_current_item_changed(item: InventoryItem):
+	$Texture.texture = item.sprite
+	
+	if (item.name == "Empty"):
+		$Amount.text = ""
+	else:
+		$Amount.text = str(_inventory.items[item])
+		
+		
+func _on_items_updated(item: InventoryItem, new_amount: int):
+	if (_inventory.current_item != item):
+		return
+	$Amount.text = str(new_amount)
