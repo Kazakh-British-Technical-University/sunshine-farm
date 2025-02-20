450306e227deacef38cd5fc626fd0677082d60f3
Dmitriy Tuchashvili <pechorin211@gmail.com>
2025-02-20 16:10:38 +0500

    added smooth camera

---
+++ scenes/player/smooth_follower.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var target: Node2D
+
+var _last_position: Vector2
+
+
+func _process(delta):
+	global_position = lerp(_last_position, target.global_position, 0.1)
+	_last_position = global_position
