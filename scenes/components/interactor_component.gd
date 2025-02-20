ab98d4fc18bc2b345114ea80fec7f6a5c8b5b95b
Dmitriy Tuchashvili <pechorin211@gmail.com>
2025-02-20 11:35:02 +0500

    fixed highlight

--- scenes/components/interactor_component.gd
+++ scenes/components/interactor_component.gd
@@ -24,6 +24,7 @@ func _process(_delta):
 	
 	_highlight.visible = true
 	_highlight.global_position = _get_priority_interactable().global_position
+	_highlight.global_rotation = 0
 
 
 func interact(origin):
